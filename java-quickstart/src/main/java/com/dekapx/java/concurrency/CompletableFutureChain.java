7a6552dcffd35b83173d3dd5d1fde174a226b124
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-01-23 22:43:20 +0000

    code clean up

---
+++ java-quickstart/src/main/java/com/dekapx/java/concurrency/CompletableFutureChain.java
@@ -0,0 +1,26 @@
+package com.dekapx.java.concurrency;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureChain {
+    public static void main(String[] args) {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> 10)
+                .thenApplyAsync(result -> multiply(result, 2))
+                .thenApplyAsync(result -> add(result, 5));
+
+        future.thenAccept(result -> print(result));
+        future.join();
+    }
+
+    private static Integer add(Integer arg, Integer value) {
+        return arg + value;
+    }
+
+    private static Integer multiply(Integer arg, Integer value) {
+        return arg * value;
+    }
+
+    private static void print(Integer arg) {
+        System.out.println(arg);
+    }
+}
