8a021cac56d4353db44c74eeba261bdb1253cad8
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-01-23 14:50:48 +0000

    code clean up

--- java-quickstart/src/main/java/com/dekapx/java/concurrency/CompletableFutureDemo.java
+++ java-quickstart/src/main/java/com/dekapx/java/concurrency/CompletableFutureDemo.java
@@ -1,20 +1,36 @@
 package com.dekapx.java.concurrency;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class CompletableFutureDemo {
-    public static void main(String[] args) {
-        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return "Hello, world!";
+    public static void main(String[] args) throws Exception {
+        CompletableFuture<List<String>> future = CompletableFuture.supplyAsync(() -> {
+            sleep(2);
+            return stepOne();
         });
 
-        future.thenAccept(result -> System.out.println(result));
-        future.join();
+        future.thenAccept(result -> stepTwo(result));
+        future.get();
+    }
+
+    private static List<String> stepOne() {
+        return List.of("Java", "Kotlin", "Python");
+    }
+
+
+    private static void stepTwo(List<String> elements) {
+        for (String element: elements) {
+            System.out.println(element);
+        }
+    }
+
+    private static void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
