d01a6eec5055effe1e16c07a5f0a97c36fe4f1c5
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-04-11 16:07:19 +0100

    code clean up

---
+++ java-quickstart/src/main/java/com/dekapx/java/function/UnaryBinaryOperatorDemo.java
@@ -0,0 +1,23 @@
+package com.dekapx.java.function;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class UnaryBinaryOperatorDemo {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        Consumer<String> c = arg -> System.out.println(arg);
+        c.accept(str);
+
+        Supplier<String> s = () -> str;
+        System.out.println(s.get());
+
+        UnaryOperator<Integer> sqrt = num -> num * num;
+        System.out.println(sqrt.apply(5));
+
+        BinaryOperator<Integer> sum = (num1, num2) -> num1 + num2;
+        System.out.println(sum.apply(5, 7));
+    }
+}
