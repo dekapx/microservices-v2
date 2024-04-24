b12ab5f087778b2ee3201196a8e481fa26c849d9
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-04-24 14:43:28 +0100

    code clean up

---
+++ java-quickstart/src/main/java/com/dekapx/java/algorithms/ArraysDemo.java
@@ -0,0 +1,29 @@
+package com.dekapx.java.algorithms;
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        int[] elements = {4, 8, 3, 7, 1, 9};
+        traverse(elements);
+        System.out.println("");
+        int number = 3;
+        int index = findIndex(elements,number);
+        System.out.println(STR."Number: \{number} found at index: \{index}");
+    }
+
+    private static void traverse(int[] elements) {
+        for (int i = 0; i < elements.length; i ++) {
+            System.out.print(elements[i]);
+        }
+    }
+
+    private static int findIndex(int[] elements, int number) {
+        int index = -1;
+        for (int i = 0; i < elements.length; i ++) {
+            if (number == elements[i]) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
