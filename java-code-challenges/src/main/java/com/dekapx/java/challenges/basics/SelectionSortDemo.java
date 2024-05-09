e65d0d7143db0b05e707b97871d1b6b5bd977269
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-05-09 17:05:49 +0100

    code clean up

--- java-code-challenges/src/main/java/com/dekapx/java/challenges/basics/SelectionSortDemo.java
+++ java-code-challenges/src/main/java/com/dekapx/java/challenges/basics/SelectionSortDemo.java
@@ -5,9 +5,26 @@ import java.util.Arrays;
 public class SelectionSortDemo {
     public static void main(String[] args) {
         int[] elements = {2, 7, 5, 8, 1, 9, 6};
-
-        printElements(elements);
+        printElements(sortElements(elements));
     }
+
+    private static int[] sortElements(int[] elements) {
+        for (int i = 0; i < elements.length; i ++) {
+            for (int j = i; j < elements.length; j ++) {
+                if (elements[i] > elements[j]) {
+                    swap(elements, i, j);
+                }
+            }
+        }
+        return elements;
+    }
+
+    private static void swap(int[] elements, int i, int j) {
+        int tmp = elements[i];
+        elements[i] = elements[j];
+        elements[j] = tmp;
+    }
+
 
     private static void printElements(int[] elements) {
         Arrays.stream(elements).forEach(i -> System.out.print(STR."\{i} "));
