008fa132d2707c2b54b560281b29c6b7f9b70f1f
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2024-09-27 12:35:33 +0100

    code clean up

--- java-code-challenges/src/main/java/com/dekapx/java/challenges/basics/SumOfNumbers.java
+++ java-code-challenges/src/main/java/com/dekapx/java/challenges/basics/SumOfNumbers.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SumOfNumbers {
-    void main(String[] args) {
+    public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
         int sum = numbers
                 .stream()
